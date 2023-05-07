d16c80e7fc4de677bcfe1bbef1979fc7a8faa5bb
mmsqe <mavis@crypto.com>
2023-05-07 22:14:47 +0800

    add getter setter for client state

--- modules/core/02-client/types/types.go
+++ modules/core/02-client/types/types.go
@@ -1,5 +1,7 @@
 package types
 
+import codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+
 // Signer is an interface that defines a method for getting the address of the account that signed a message.
 type Signer interface {
 	// GetSigner returns the address of the account that signed the message.
@@ -24,4 +26,42 @@ func (m *MsgUpgradeClient) GetSigner() string {
 // GetSigner returns the address of the account that submitted the misbehaviour evidence.
 func (m *MsgSubmitMisbehaviour) GetSigner() string {
 	return m.Signer
+}
+
+// ClientStateMsg is an interface that defines methods for getting and setting the client state of a message.
+type ClientStateMsg interface {
+	// GetClientState returns the byte slice representation of the client state included in the message.
+	// Returns nil if the client state is not set.
+	GetClientState() []byte
+
+	// SetClientState sets the client state in the message to the given value.
+	SetClientState(state *codectypes.Any)
+}
+
+// GetClientState returns the byte slice representation of the client state included in the create client message.
+// Returns nil if the client state is not set.
+func (m *MsgCreateClient) GetClientState() []byte {
+	if m.ClientState == nil {
+		return nil
+	}
+	return m.ClientState.Value
+}
+
+// SetClientState sets the client state in the create client message to the given value.
+func (m *MsgCreateClient) SetClientState(state *codectypes.Any) {
+	m.ClientState = state
+}
+
+// GetClientState returns the byte slice representation of the client state included in the upgrade client message.
+// Returns nil if the client state is not set.
+func (m *MsgUpgradeClient) GetClientState() []byte {
+	if m.ClientState == nil {
+		return nil
+	}
+	return m.ClientState.Value
+}
+
+// SetClientState sets the client state in the upgrade client message to the given value.
+func (m *MsgUpgradeClient) SetClientState(state *codectypes.Any) {
+	m.ClientState = state
 }
--- modules/core/03-connection/types/types.go
+++ modules/core/03-connection/types/types.go
@@ -1,5 +1,7 @@
 package types
 
+import codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+
 // Signer is an interface that defines a method for getting the address of the account that signed a message.
 type Signer interface {
 	// GetSigner returns the address of the account that signed the message.
@@ -24,4 +26,32 @@ func (m *MsgConnectionOpenAck) GetSigner() string {
 // GetSigner returns the address of the account that signed the message to confirm a connection opening.
 func (m *MsgConnectionOpenConfirm) GetSigner() string {
 	return m.Signer
+}
+
+// GetClientState returns the byte slice representation of the client state included in the connection open try message.
+// Returns nil if the client state is not set.
+func (m *MsgConnectionOpenTry) GetClientState() []byte {
+	if m.ClientState == nil {
+		return nil
+	}
+	return m.ClientState.Value
+}
+
+// SetClientState sets the client state in the connection open try message to the given value.
+func (m *MsgConnectionOpenTry) SetClientState(state *codectypes.Any) {
+	m.ClientState = state
+}
+
+// GetClientState returns the byte slice representation of the client state included in the connection open acknowledgement message.
+// Returns nil if the client state is not set.
+func (m *MsgConnectionOpenAck) GetClientState() []byte {
+	if m.ClientState == nil {
+		return nil
+	}
+	return m.ClientState.Value
+}
+
+// SetClientState sets the client state in the connection open acknowledgement message to the given value.
+func (m *MsgConnectionOpenAck) SetClientState(state *codectypes.Any) {
+	m.ClientState = state
 }
